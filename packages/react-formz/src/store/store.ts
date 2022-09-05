0cd06578451013f30654138c5f9476420d8969c8
jerrywithaz <dev@jerrywithaz.com>
2022-09-05 14:24:48 -0500

    fix zustand immer/persist types

--- packages/react-formz/src/store/store.ts
+++ packages/react-formz/src/store/store.ts
@@ -1,57 +1,57 @@
 import create from "zustand";
 import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
-import { FormzMiddlewares, FormzStore } from "./store.types";
+import { FormzStore } from "./store.types";
 import { mergeDeepLeft } from "ramda";
 
-const middlewares = ((fn) =>
-  persist<FormzStore>(immer(fn), {
-    name: "react-formz",
-    getStorage: () => localStorage,
-    merge: (persistedState, currentState) =>
-      mergeDeepLeft(
-        persistedState as FormzStore["forms"],
-        currentState
-      ) as FormzStore,
-    partialize: (state) => ({
-      forms: Object.keys(state.forms).reduce((forms, key) => {
-        const form = state.forms[key];
-
-        if (form?.saveDrafts) {
-          return { ...forms, [key]: form };
-        }
-        return forms;
-      }, {}),
-    }),
-    onRehydrateStorage: () => {},
-  })) as FormzMiddlewares;
-
 export const formzStore = create<FormzStore>()(
-  middlewares((set) => ({
-    forms: {},
-    addForm: (id, initialValues, saveDrafts) => {
-      set((state) => {
-        if (state.forms[id] === undefined) {
-          state.forms[id] = {
-            values: initialValues,
-            touched: {},
-            errors: {},
-            isSubmitting: false,
-            isValidating: false,
-            submitCount: 0,
-            initialized: true,
-            isInvalid: false,
-            saveDrafts: saveDrafts,
-          };
-        }
-      });
-    },
-    removeForm: (id) => {
-      set((state) => {
-        if (state.forms[id]) delete state.forms[id];
-      });
-    },
-  }))
+  persist(
+    immer((set) => ({
+      forms: {},
+      addForm: (id, initialValues, saveDrafts) => {
+        set((state) => {
+          if (state.forms[id] === undefined) {
+            state.forms[id] = {
+              values: initialValues,
+              touched: {},
+              errors: {},
+              isSubmitting: false,
+              isValidating: false,
+              submitCount: 0,
+              initialized: true,
+              isInvalid: false,
+              saveDrafts: saveDrafts,
+            };
+          }
+        });
+      },
+      removeForm: (id) => {
+        set((state) => {
+          if (state.forms[id]) delete state.forms[id];
+        });
+      },
+    })),
+    {
+      name: "react-formz",
+      getStorage: () => localStorage,
+      merge: (persistedState, currentState) =>
+        mergeDeepLeft(
+          persistedState as FormzStore["forms"],
+          currentState
+        ) as FormzStore,
+      partialize: (state) => ({
+        forms: Object.keys(state.forms).reduce((forms, key) => {
+          const form = state.forms[key];
+
+          if (form?.saveDrafts) {
+            return { ...forms, [key]: form };
+          }
+          return forms;
+        }, {}),
+      }),
+      onRehydrateStorage: () => {},
+    }
+  )
 );
 
 export const useFormz = formzStore;
--- packages/react-formz/src/store/store.types.ts
+++ packages/react-formz/src/store/store.types.ts
@@ -1,23 +1,5 @@
-import { StoreMutatorIdentifier, StateCreator } from "zustand";
 import { FormzState, FormzValues } from "../types/form";
-/**
- * Typed zustand middlewares thanks to: https://github.com/pmndrs/zustand/issues/1242#issuecomment-1231925031
- */
-export type FormzMiddlewares = <
-  T,
-  Mps extends [StoreMutatorIdentifier, unknown][] = [],
-  Mcs extends [StoreMutatorIdentifier, unknown][] = []
->(
-  f: StateCreator<
-    T,
-    [...Mps, ["zustand/persist", T], ["zustand/immer", T]],
-    Mcs
-  >
-) => StateCreator<
-T,
-  Mps,
-  [["zustand/persist", T], ["zustand/immer", T], ...Mcs]
->;
+
 export interface FormzStore {
   forms: Record<string, FormzState<any>>;
   addForm: <Values extends FormzValues>(

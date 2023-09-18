27f0cbbd864e08edf999b0b2986ad226933a6215
jerrywithaz <dev@jerrywithaz.com>
2023-09-17 19:10:58 -0500

    Update: add tokens

--- packages/react-formz-validators/utils/toNestedErrors.ts
+++ packages/react-formz-validators/utils/toNestedErrors.ts
@@ -1,18 +1,4 @@
-import { FormzErrors, FormzValues } from "@zerry/react-formz-core";
-
-function tryRequire(pkg: string) {
-    try {
-        return require(pkg);
-    } catch (error) {
-        return undefined;
-    }
-}
-
-const set = tryRequire("@zerry/react-formz")?.set ?? tryRequire("@zerry/react-formz-native")?.set ?? undefined;
-
-if (!set) {
-    throw new Error("@zerry/react-formz or @zerry/react-formz-native are not installed or could not be found.")
-}
+import { FormzErrors, FormzValues, set } from "@zerry/react-formz-core";
 
 function toNestedErrors<Values extends FormzValues>(errors: FormzErrors<Values>) {
     const nestedErrors: FormzErrors<Values> = {};
